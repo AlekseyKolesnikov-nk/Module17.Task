f600353bbae32f352426e01a0a8b07b5932f6f23
AlekseyKolesnikov-nk <Aleksey.Kolesnikov@evraz.com>
2024-12-08 19:49:32 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Module17.Task;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Calculator calculator1 = new Calculator(new SalaryAccount(600));
+        calculator1.Calculate();
+        Console.WriteLine();
+
+        Calculator calculator2 = new Calculator(new SalaryAccount(1200));
+        calculator2.Calculate();
+        Console.WriteLine();
+
+        Calculator calculator3 = new Calculator(new OrdinaryAccount(600));
+        calculator3.Calculate();
+        Console.WriteLine();
+
+        Calculator calculator4 = new Calculator(new OrdinaryAccount(1200));
+        calculator4.Calculate();
+        Console.WriteLine();
+    }
+}
+
---
+++ SalaryAccaunt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module17.Task
+{
+    public class SalaryAccount : IAccount
+    {
+        // тип учетной записи
+        public string Type { get; set; }
+
+        // баланс учетной записи
+        public double Balance { get; set; }
+
+        // процентная ставка
+        public double Interest { get; set; }
+
+        public SalaryAccount(double balance)
+        {
+            Type = "Зарплатный";
+            Balance = balance;
+        }
+        
+        // Метод для расчета процентной ставки
+        public void CalculateInterest()
+        {
+            // расчет процентной ставки зарплатного аккаунта
+            // при любом остатке на счете ставка 0,5
+            this.Interest = this.Balance * 0.5;
+        }
+    }
+}
